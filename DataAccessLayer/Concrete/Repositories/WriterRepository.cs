ffcaa6ec3988f2bf4cd860767ab13d44243affa1
Hilalbiyik <hilalbyk396@gmail.com>
2021-04-23 15:57:56 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Repositories/WriterRepository.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLayer.Abstract;
+using EntitiyLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class WriterRepository : IWriterDal
+    {
+        public void Delete(Writer p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Writer p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Writer> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Writer> List(Expression<Func<Writer, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Writer p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
